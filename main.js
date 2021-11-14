aeabd6ccc26616eae3c32e6c02da994e9d8fd359
liang xiaoman <18814127191@163.com>
2021-11-14 22:40:46 +0800

    添加自动更新

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow ,Menu} = require('electron')
+const { app, BrowserWindow ,Menu, dialog} = require('electron')
+const { autoUpdater} = require('electron-updater')
 const isDev = require('electron-is-dev')
 const path = require('path')
 function createWindow() {
@@ -15,6 +16,49 @@ function createWindow() {
     win.loadURL(urlLocation)
 }
 app.whenReady().then(() => {
+    if(isDev){
+		autoUpdater.updateConfigPath = path.join(__dirname, 'dev-app-update.yml')
+	}
+    // 检查更新
+    autoUpdater.autoDownload = false;
+    autoUpdater.checkForUpdatesAndNotify();
+    autoUpdater.on('error', (error) => {
+        console.log("error:",error)
+        // dialog.showErrorBox('Error:',error==null?'unkown':error)
+    })
+    autoUpdater.on('checking-for-update',()=>{
+        console.log("checking-for-update...")
+    })
+    autoUpdater.on('update-available',()=>{
+        dialog.showMessageBox({
+            type:'info',
+            title:'应用有新的版本',
+            message:'发现新版本，是否现在更新？',
+            buttons:['是','否']
+        },(buttonIndex)=>{
+            if(buttonIndex===0){
+                autoUpdater.downloadUpdate()
+            }
+        })
+    })
+    autoUpdater.on('update-not-available',()=>{
+        dialog.showMessageBox({
+            title:'没有新的版本',
+            message:'当前已经是最新版本'
+        })
+    })
+    autoUpdater.on('download-progress',(progressObj)=>{
+        console.log("progressObj:",progressObj)
+    })
+    autoUpdater.on('update-downloaded',()=>{
+        dialog.showMessageBox({
+            title:'安装更新',
+            message:'更新下载完毕，应用将重启并进行安装'
+        },()=>{
+            setImmediate(()=>{autoUpdater.quitAndInstall()})
+        })
+    })
+
     createWindow()
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
